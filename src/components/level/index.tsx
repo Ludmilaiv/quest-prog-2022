1ee9290c1b90449e85cb4a1e7d14b0430fb3c84f
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-04-20 13:11:31 +0700

    fix router

--- src/components/level/index.tsx
+++ src/components/level/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useSearchParams} from 'react-router-dom';
 import {Level1} from './components/level1';
 import './style.sass';
+
+function CangeLevel() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
+  const level = searchParams.get('level');
+  return (
+    <div className="App">
+      {level === 'fd347' ? <Level1 /> : 'Error 404. Page not found'}
+    </div>
+  );
+}
 
 export const Level = () => {
   return (
     <div className='level'>
       <Router>
         <Routes>
-          <Route path='/fd347' element={<Level1 />}/>
+          <Route path='/' element={<CangeLevel />}/>
         </Routes>
       </Router>
     </div>
